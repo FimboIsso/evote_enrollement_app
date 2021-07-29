b58074f48ec9e41d12379907f1c9d868f99cc145
FimboIsso <fimbo.isso@gmail.com>
2021-07-29 10:43:03 +0200

    first commit

---
+++ app/src/main/java/issonet/com/vote_enronlement_2019_new/aboutUsActivity.java
@@ -0,0 +1,13 @@
+package issonet.com.vote_enronlement_2019_new;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class aboutUsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_us);
+    }
+}
